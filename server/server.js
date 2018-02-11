f084d994c39048aa737b45d4140c23a63a6c56fa
Martin Kocsis <egykettoharo@gmail.com>
2018-02-11 21:49:15 +0100

    First commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path      = require('path');
+const express   = require('express');
+
+const publicPath    = path.join(__dirname, '../public');
+const port          = process.env.PORT || 3000;
+
+//console.log(__dirname + '/../public');
+//console.log(publicPath);
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+//app.get('/', (req, res) => {
+//    res.send();
+//});
+
+app.listen(port, () => {
+    console.log('Started on port ' + port);
+});
